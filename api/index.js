fed39b10c07f5c8131bd01929fb6612315e24cdc
William Johnson <bill@Williams-MacBook-Pro.local>
2015-11-09 21:28:32 -0500

    API will now automatically match folder structure in /api folder.

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
+import glob from 'glob';
+
 export default (app) => {
-  app.use('/api/todos', require('./todos'));
+  let files = glob.sync( "api/**")
+    .filter(file => file !== 'api' && file !== 'api/index.js') // Ignore base dir and index.js
+    .filter(file => file.slice(-2) === 'js')                   // Filter out folders.
+    .map(file => file.slice(3, -3));                           // Remove 'api' from the front and '.js' from the back.
+
+  // Api now matches folder structure!
+  files.map(file => app.use('/api' + file, require('.' + file))); 
 };
