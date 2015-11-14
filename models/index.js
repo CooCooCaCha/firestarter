dbcc1cc6faa8732e20e6f161efb63f3331eb2a29
William Johnson <bill@Williams-MacBook-Pro.local>
2015-11-14 18:50:15 -0500

    Revamped database connection and migration system.

--- models/index.js
+++ models/index.js
@@ -1,19 +1,41 @@
 var Sequelize = require('sequelize');
+var Umzug     = require('umzug');
 var glob      = require('glob');
 var path      = require('path');
-var config    = require('config').get('development');
+var config    = require('config').get('database');
 var db        = {};
 
-// initialize database connection
 var sequelize = new Sequelize(
   config.name,
   config.username,
   config.password,
   {
-    host: config.host,
-    dialect: config.dialect
+    host: process.env.POSTGRES_PORT_5432_TCP_ADDR,
+    dialect: "postgres",
+    pool: {
+      maxConnections: 100,
+      maxIdleTime: 1000
+    }
   }
 );
+
+// initialize database connection
+Sequelize.Promise.onPossiblyUnhandledRejection((error, promise) => {
+  if(error.name === "SequelizeConnectionRefusedError") {
+    sequelize.authenticate();
+  }
+});
+
+var umzug = new Umzug({
+  sequelize: sequelize,
+  migrations: {
+    params: [sequelize.getQueryInterface(), sequelize.constructor, () => {
+      throw new Error('Migration tried to use old style "done" callback. Please upgrade to "umzug" and return a promise instead.');
+    }],
+    path: './migrations',
+    pattern: /\.js$/
+  }
+});
 
 glob.sync('models/**.js')
   .filter(file => file !== 'models/index.js')
@@ -28,9 +50,10 @@ Object.keys(db).forEach(modelName => {
   }
 });
 
+umzug.up().then(migrations => {
+  migrations.forEach(migration => console.info("MIGRATION: " + migration.file));
+});
+
 sequelize.sync();
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+export default db;
